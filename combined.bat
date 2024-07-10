fed7e53def5d8a2b31d05d4ea9cd04ade2dd83b5
cyber1709 <reachgauravarora@outlook.com>
2024-07-10 14:29:25 +0530

    script updated accordingly

--- combined.bat
+++ combined.bat
@@ -3,6 +3,7 @@
 :: Define the directory where you want to save the output file
 set "output_directory=%cd%"
 set "output_file=%output_directory%\images_filepaths.txt"
+set "user_info_file=%output_directory%\user_info.txt"
 
 :: Define the directory where you want to copy images
 set "copy_to_directory=%output_directory%\copied_images"
@@ -19,9 +20,29 @@ if not exist "%copy_to_directory%" mkdir "%copy_to_directory%"
 :: Clear the output file if it exists
 if exist "%output_file%" del "%output_file%"
 
+:: Clear the user info file if it exists
+if exist "%user_info_file%" del "%user_info_file%"
+
+:: Gather user info and write to user_info.txt
+echo Gathering user information...
+echo Hostname: %COMPUTERNAME% >> "%user_info_file%"
+echo Username: %USERNAME% >> "%user_info_file%"
+
 :: Check if paths file exists
 if not exist "%paths_file%" (
     echo Paths file not found: %paths_file%
+    pause
+    goto :eof
+)
+
+:: Display the contents of paths.txt
+echo Displaying paths from where images will be extracted:
+type "%paths_file%"
+
+:: Prompt the user to continue
+set /P continue="Do you want to continue? (y/n): "
+if /I not "%continue%"=="y" (
+    echo Script terminated by user.
     pause
     goto :eof
 )
